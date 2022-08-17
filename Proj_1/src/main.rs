ef32fde19893058539b946bc0dafc508e3191133
JoeKenn1118 <jdk398@msstate.edu>
2022-08-17 11:04:43 -0500

    Started New Proj

---
+++ Proj_1/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, Welcome to the Land of {}", "Fildal");
+    println!("Lets start the game");
+
+    
+}
---
+++ Proj_1/target/debug/deps/Proj_1-50be7bcdcd42d6b6.d
@@ -0,0 +1,5 @@
+c:\Users\Joseph\The_Insanity_Project\Proj_1\target\debug\deps\Proj_1-50be7bcdcd42d6b6.rmeta: src\main.rs
+
+c:\Users\Joseph\The_Insanity_Project\Proj_1\target\debug\deps\Proj_1-50be7bcdcd42d6b6.d: src\main.rs
+
+src\main.rs:
