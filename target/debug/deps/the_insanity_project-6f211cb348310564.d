1ca730bb48ca9c9f6bd853e6e56bf873555f3c35
JoeKenn1118 <jdk398@msstate.edu>
2022-08-18 11:58:11 -0500

    Second Commit for build objects

--- target/debug/deps/the_insanity_project-6f211cb348310564.d
+++ target/debug/deps/the_insanity_project-6f211cb348310564.d
@@ -1,7 +1,5 @@
-c:\Users\Joseph\The_Insanity_Project\target\debug\deps\the_insanity_project-6f211cb348310564.rmeta: src\lib.rs src\characters.rs src\general_info.rs
+c:\Users\Joseph\The_Insanity_Project\target\debug\deps\the_insanity_project-6f211cb348310564.rmeta: src\lib.rs
 
-c:\Users\Joseph\The_Insanity_Project\target\debug\deps\the_insanity_project-6f211cb348310564.d: src\lib.rs src\characters.rs src\general_info.rs
+c:\Users\Joseph\The_Insanity_Project\target\debug\deps\the_insanity_project-6f211cb348310564.d: src\lib.rs
 
 src\lib.rs:
-src\characters.rs:
-src\general_info.rs:
